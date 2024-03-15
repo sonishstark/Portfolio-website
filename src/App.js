314b99898f679e270a59428fa1fe79b04c9d11fc
Sonish Stark <sonishstark@gmail.com>
2024-03-15 15:29:05 -0400

    1st commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from "./utils/Themes.js";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import ProjectDetails from "./components/ProjectDetails";
+import styled from "styled-components";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(343.07deg, rgb(23 92 230 / 7%) 5.71%, #0b1e30f7 64.83%);
+  width: 100%;
+`;
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+            <Contact />
+          </Wrapper>
+          <Footer />
+          {openModal.state && (
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          )}
+        </Body>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
