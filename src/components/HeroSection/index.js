04855e9327e2b985d80b60a625396b6d92821400
Sonish Stark <sonishstark@gmail.com>
2024-03-17 05:58:44 -0400

    final done

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -50,7 +50,7 @@ const HeroSection = () => {
           </HeroLeftContainer>
 
           <HeroRightContainer id="Right">
-            <Img src={HeroImg} alt="hero-image" />
+            <Img src="https://github.com/sonishstark/Portfolio-website/blob/main/src/images/HeroImage.png?raw=true" alt="hero-image" />
           </HeroRightContainer>
         </HeroInnerContainer>
       </HeroContainer>
